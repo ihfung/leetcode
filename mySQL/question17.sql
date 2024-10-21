44152ad62b9bdeea43bb219f5a55486ee618ec7a
ihleung <ihfungleung@gmail.com>
2024-10-21 16:10:18 -0400

    added more questions

---
+++ mySQL/question17.sql
@@ -0,0 +1,105 @@
+-- Project Employees I
+-- Table: Project
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+-- Table: Employee
+
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+-- Each row of this table contains information about one employee.
+ 
+
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 1                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+-- Output: 
+-- +-------------+---------------+
+-- | project_id  | average_years |
+-- +-------------+---------------+
+-- | 1           | 2.00          |
+-- | 2           | 2.50          |
+-- +-------------+---------------+
+-- Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+
+create table project (
+    project_id int,
+    employee_id int
+);
+
+insert into project (project_id, employee_id) values
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 4);
+
+create table employee (
+    employee_id int,
+    name varchar(100),
+    experience_years int
+);
+
+insert into employee (employee_id, name, experience_years) values
+(1, 'Khaled', 3),
+(2, 'Ali', 2),
+(3, 'John', 1),
+(4, 'Doe', 2);
+
+select p.project_id, 
+round(sum(e.experience_years) / count(p.employee_id), 2) as average_years
+from project p join employee e
+on p.employee_id = e.employee_id
+group by p.project_id;
+
+
+--when to use left join 
+--when you want to include all the rows from the left table and only the matching rows from the right table
+
+--when to use right join
+--when you want to include all the rows from the right table and only the matching rows from the left table
+
+--when to use join 
+--when you want to include only the matching rows from both tables
