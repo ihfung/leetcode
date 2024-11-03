02f8c17f3de3a0805bdcd5b5371585d4c5d68b96
ihleung <ihfungleung@gmail.com>
2024-11-03 16:34:22 -0500

    updated q21

--- mySQL/question21.sql
+++ mySQL/question21.sql
@@ -1,11 +1,10 @@
-select round(100.0 * sum(case when first.order_date = first.customer_pref_delivery_date
-then 1 else 0 end) / count(*), 2) as immediate_percentage
-from (
-select customer_id, delivery_id, order_date, customer_pref_delivery_date, 
-row_number() over (partition by customer_id 
-order by order_date) as rn
-from delivery
-) as first
-where rn = 1;
+select round(avg(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage  -- round to 2 decimal places and the avg of order date which is customer prefer delivery date
+from 
+(select customer_id, 
+min(order_date) as order_date, 
+min(customer_pref_delivery_date) as customer_pref_delivery_date 
+from Delivery 
+group by customer_id )  --this select statement is used to get the customer_id, order_date and customer_pref_delivery_date from the Delivery table and group by customer_id
+as first_orders; --alias name for the select statement is first_orders
 
 
